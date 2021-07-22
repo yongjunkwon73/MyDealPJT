31dee165f42c911e1a378195aedf425f5d51dcd3
yongjunkwon73 <85722789+yongjunkwon73@users.noreply.github.com>
2021-07-22 12:38:57 +0900

    Add files via upload

---
+++ Consign/src/main/java/mydealprj/ConsignCancelled.java
@@ -0,0 +1,58 @@
+package mydealprj;
+
+public class ConsignCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long puId;
+    private String conStatus;
+    private String payStatus;
+    private String conDate;
+    private String conCancelDate;
+
+    public ConsignCancelled(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getPuId() {
+        return puId;
+    }
+
+    public void setPuId(Long puId) {
+        this.puId = puId;
+    }
+    public String getConStatus() {
+        return conStatus;
+    }
+
+    public void setConStatus(String conStatus) {
+        this.conStatus = conStatus;
+    }
+    public String getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(String payStatus) {
+        this.payStatus = payStatus;
+    }
+    public String getConDate() {
+        return conDate;
+    }
+
+    public void setConDate(String conDate) {
+        this.conDate = conDate;
+    }
+    public String getConCancelDate() {
+        return conCancelDate;
+    }
+
+    public void setConCancelDate(String conCancelDate) {
+        this.conCancelDate = conCancelDate;
+    }
+}
