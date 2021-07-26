1b0e3b7a4c1ddc732da607baaaabb97b320af537
yongjunkwon73 <yonggari99@daum.net>
2021-07-26 22:55:17 +0000

    수정0706

--- Purchase/src/main/java/mydealprj/external/BillingService.java
+++ Purchase/src/main/java/mydealprj/external/BillingService.java
@@ -8,11 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
-
+//  원소스 
+ /*
 @FeignClient(name="Billing", url="http://Billing:8080")
 public interface BillingService {
     @RequestMapping(method= RequestMethod.GET, path="/billings")
     public void pay(@RequestBody Billing billing);
-
-}
-
+}  */
+  
+ // pu 서비스에서 바라보는 billing 서비스 url 일부분을 ConfigMao 사용하요 구현
+ 
+@FeignClient(name="Billing", url="http://${api.url.Biiling}")
+public interface BillingService {
+    @RequestMapping(method= RequestMethod.GET, path="/billings")
+    public void pay(@RequestBody Billing billing);
+   */
+} 
